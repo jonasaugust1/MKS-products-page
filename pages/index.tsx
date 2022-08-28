bb51d7ad2eb4ac742a87055f20afcd02d941a91c
Jonas Augusto <jonasaugusto99@gmail.com>
2022-08-28 01:29:17 -0300

    :sparkles: feat: Add New Resources

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,11 @@ import { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/header/header'
+import Inventory from '../components/inventory/inventory'
 import Footer from '../components/footer/footer'
-import {getProducts} from '../utils/products'
+import Cart from '../components/cart/cart'
+import {getProducts} from '../utils/getProducts'
 import {GetProductResults, Product} from '../types'
-import utilStyles from '../styles/utils.module.css'
-
-
 
 
 export const getStaticProps: GetStaticProps = async (context) => {
@@ -25,43 +24,8 @@ const Home: NextPage<{products: Product[]}> = ({products}) => {
    <div>
       <Header home/>
       <main>
-        <div className={utilStyles.productsContainer}>
-            {products.map(product => (
-                <div key={product.id} className={utilStyles.productCard}>
-                  <div className={utilStyles.imageContainer}>
-                  <Image
-                    loader={() => product.photo}
-                    src={product.photo}
-                    alt={product.name}
-                    width={150}
-                    height={150}
-                  />
-                  </div>
-                  
-                  <div className={utilStyles.infoContainer}>
-                    <div className={utilStyles.nameContainer}>
-                      <h5>{product.name}</h5>
-                    </div>
-                    <div className={utilStyles.priceContainer}>
-                      <span>R${product.price}</span>
-                    </div>
-                  </div>
-                  
-
-                  <p>{product.description}</p>
-                 
-                  <button className={utilStyles.buyBtn}>
-                      <Image
-                        src='/images/shopping-bag.svg'
-                        alt='buy'
-                        width={20}
-                        height={20}
-                      />
-                      Comprar
-                    </button>
-                </div>
-            ))}
-        </div>
+        <Inventory products={products}/>
+        <Cart/>      
       </main>
       <Footer/>
    </div>
